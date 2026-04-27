160a80201b5f8aa995cc70f8211683a42a50b2a4
productionsgclef-debug <productionsgclef@gmail.com>
2026-04-27 16:09:27 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'connectly-cache-v1';
+const urlsToCache = ['/'];
+
+// Install the service worker and cache the home page
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
